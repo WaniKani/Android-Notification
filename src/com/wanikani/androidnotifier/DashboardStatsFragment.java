ea6ca405779e2f27f64b2b136dd5ed4576afebed
Alberto Cuda <alberto.cuda@gmail.com>
2013-08-06 18:20:35 +0200

    Added documentation

--- src/com/wanikani/androidnotifier/DashboardStatsFragment.java
+++ src/com/wanikani/androidnotifier/DashboardStatsFragment.java
@@ -10,8 +10,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+/* 
+ *  Copyright (c) 2013 Alberto Cuda
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * A tab holding the dashboard and stats items. They are kind of similar,
+ * so it is probably a good enhancement to group them, when using a tablet.
+ */
 public class DashboardStatsFragment extends TabsFragment {
-	
+
+	/// The parent view
 	View parent;
 	
 	/**
@@ -30,7 +52,8 @@ public class DashboardStatsFragment extends TabsFragment {
 				
 		return parent;
     }
-	
+
+	@Override
 	public void addChildren (List<Tab> fragments)
 	{
 		FragmentManager mgr;
@@ -47,6 +70,11 @@ public class DashboardStatsFragment extends TabsFragment {
 		return R.string.tag_dashboard; 
 	}
 
+	/**
+	 * This class overrides the stock spin method, because we want only
+	 * the dashboard to show the spinner. I think it's better this way.
+	 * @param enable to enable spinner
+	 */
 	@Override
 	public void spin (boolean enable)
 	{
--- src/com/wanikani/androidnotifier/TabsFragment.java
+++ src/com/wanikani/androidnotifier/TabsFragment.java
@@ -6,15 +6,44 @@ import java.util.Vector;
 import android.app.Activity;
 import android.support.v4.app.Fragment;
 
+/* 
+ *  Copyright (c) 2013 Alberto Cuda
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * A fragment that contains other fragments. This is useful to group
+ * more than one fragment into a single tab when the device is large enough.
+ */
 public abstract class TabsFragment extends Fragment implements Tab {
 	
+	/// The fragments
 	List<Tab> fragments;	
 
+	/**
+	 * Constructor
+	 */
 	public TabsFragment ()
 	{
 		fragments = new Vector<Tab> ();
 	}
 	
+	/**
+	 * Called when attached. As per tab contract, we register on the main activity.
+	 * 	@param main the activity
+	 */
 	@Override
 	public void onAttach (Activity main)
 	{
@@ -23,6 +52,10 @@ public abstract class TabsFragment extends Fragment implements Tab {
 		((MainActivity) main).register (this);			
 	}
 	
+	/**
+	 * Called when the activity is starting. Obtains thew list of 
+	 * fragments, by calling {@link #addChildren(List)} on the derived class.
+	 */
 	@Override
 	public void onStart ()
 	{
@@ -33,6 +66,9 @@ public abstract class TabsFragment extends Fragment implements Tab {
 		addChildren (fragments);
 	}
 	
+	/**
+	 * Called when stopping. Clears the fragment list
+	 */
 	@Override
 	public void onStop ()
 	{
@@ -41,26 +77,35 @@ public abstract class TabsFragment extends Fragment implements Tab {
 		fragments.clear ();
 	}
 	
+	/**
+	 * This method must be implemented by derived class to provide a list of 
+	 * the fragments that we are supposed to handle
+	 * @param fragments a list that shall be populated by the subclass
+	 */
 	public abstract void addChildren (List<Tab> fragments);
 	
+	@Override
 	public void refreshComplete (DashboardData dd)
 	{
 		for (Tab t : fragments)
 			t.refreshComplete (dd);
 	}
 	
+	@Override
 	public void spin (boolean enable)
 	{
 		for (Tab t : fragments)
 			t.spin (enable);		
 	}
 	
+	@Override
 	public void flush (RefreshType rtype)
 	{
 		for (Tab t : fragments)
 			t.flush (rtype);		
 	}
 	
+	@Override
 	 public boolean scrollLock ()
 	 {
 		for (Tab t : fragments)
@@ -70,6 +115,7 @@ public abstract class TabsFragment extends Fragment implements Tab {
 		return false;
 	 }
 	 
+	@Override
 	 public boolean backButton ()
 	 {
 		for (Tab t : fragments)
