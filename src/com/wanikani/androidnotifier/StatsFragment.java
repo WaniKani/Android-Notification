40b5948d7a68cd944c62bb64d8c1e6954a7a270e
Alberto Cuda <alberto.cuda@gmail.com>
2013-10-03 21:49:32 +0200

    Merged avg and levelup sentences

--- src/com/wanikani/androidnotifier/StatsFragment.java
+++ src/com/wanikani/androidnotifier/StatsFragment.java
@@ -643,7 +643,7 @@ public class StatsFragment extends Fragment implements Tab {
 		
 		private boolean updateNextLevel (Map<Integer, Integer> days)
 		{
-			TextView tw, stw;
+			TextView tw;
 			Integer lastlup;
 			Float delay;
 			Calendar cal;
@@ -651,10 +651,8 @@ public class StatsFragment extends Fragment implements Tab {
 			
 			delay = weight (days, WEXP_NEXT);
 			tw = (TextView) parent.findViewById (R.id.tv_eta_avg);
-			stw = (TextView) parent.findViewById (R.id.tv_eta_next);
 			if (delay != null) {
-				s = beautify (delay);
-				tw.setText (main.getString (R.string.fmt_eta_avg, s));
+				s = main.getString (R.string.fmt_eta_avg, beautify (delay));
 				tw.setVisibility (View.VISIBLE);
 				
 				lastlup = cs.levelups.get (dd.level);
@@ -664,19 +662,17 @@ public class StatsFragment extends Fragment implements Tab {
 					cal.add (Calendar.DATE, lastlup);
 					delay -= ((float) System.currentTimeMillis () - cal.getTimeInMillis ()) / (24 * 3600 * 1000);
 					if (delay > 0)
-						s = main.getString (R.string.fmt_eta_next_future, beautify (delay));
+						s += ", " + main.getString (R.string.fmt_eta_next_future, beautify (delay));
 					else
-						s = main.getString (R.string.fmt_eta_next_past, beautify (-delay));
-					stw.setText (s);
-					stw.setVisibility (View.VISIBLE);
-				} else
-					stw.setVisibility (View.GONE);
+						s += ", " + main.getString (R.string.fmt_eta_next_past, beautify (-delay));
+				} 
 				
+				tw.setText (s);
+
 				return true;
 				
 			} else {
 				tw.setVisibility (View.GONE);
-				stw.setVisibility (View.GONE);
 				
 				return false;
 			}
