368bc9749547988899c30ef91f16530aaff65f6c
Alberto Cuda <alberto.cuda@gmail.com>
2013-10-27 15:37:18 +0100

    Integrated release WK Reorder 0.3

--- src/com/wanikani/androidnotifier/ReviewOrder.java
+++ src/com/wanikani/androidnotifier/ReviewOrder.java
@@ -3,14 +3,43 @@ package com.wanikani.androidnotifier;
 public class ReviewOrder {
 	
 	public static final String JS_CODE =
+"function get(id) {\r\n" + 
+"    if (id && typeof id === 'string') {\r\n" + 
+"        id = document.getElementById(id);\r\n" + 
+"    }\r\n" + 
+"    return id || null;\r\n" + 
+"}\r\n" + 
+"\r\n" + 
 "function init(){\r\n" + 
-"	console.log('start');\r\n" + 
+"	console.log('init() start');\r\n" + 
+"	var stats = $(\"#stats\")[0];\r\n" + 
+"    var t = document.createElement('div');\r\n" + 
+"    stats.appendChild(t);\r\n" + 
+"    \r\n" + 
+"    t.innerHTML = '<div id=\"wkroStatus\"><table align=\"right\"><tbody>'+\r\n" + 
+"        '<tr><td>Rad</td><td align=\"right\"><span id=\"wkroRadCount\"></span></td></tr>'+\r\n" + 
+"        '<tr><td>Kan</td><td align=\"right\"><span id=\"wkroKanCount\"></span></td></tr>'+\r\n" + 
+"        '<tr><td>Voc</td><td align=\"right\"><span id=\"wkroVocCount\"></span></td></tr>'+\r\n" + 
+"        '</tbody></table></div><div id=\"divSt\">Not Ordered!</div><button id=\"reorderBtn\" type=\"button\" onclick=\"window.wknReorder()\">Reorder!</button></div>';\r\n" + 
+"    $.jStorage.listenKeyChange(\"activeQueue\",displayUpdate);\r\n" + 
+//"	window.addEventListener('reorderWK',reorder); \r\n" + 
+"    displayUpdate();\r\n" + 
+"	console.log('init() end');\r\n" + 
+"}\r\n" + 
+"\r\n" + 
+//"function reorder(){\r\n" +
+"window.wknReorder = function() {\r\n" +
+"    console.log('reorder() start');\r\n" + 
+"    var divSt = get(\"divSt\");\r\n" + 
+"    reorderBtn.style.visibility=\"hidden\";\r\n" + 
+"    divSt.innerHTML = 'Reordering.. please wait!';\r\n" + 
+"        \r\n" + 
 "    var cur = $.jStorage.get(\"currentItem\");\r\n" + 
 "	var qt = $.jStorage.get(\"questionType\");\r\n" + 
 "	var actList = $.jStorage.get(\"activeQueue\");\r\n" + 
 "	var revList = $.jStorage.get(\"reviewQueue\");\r\n" + 
 "    \r\n" + 
-"    console.log(cur);\r\n" + 
+"    console.log('current item: '+cur);\r\n" + 
 "    var curt = cur.kan?'kan':cur.voc?'voc':'rad';\r\n" + 
 "    \r\n" + 
 "    var removedCount = 0;\r\n" + 
@@ -24,14 +53,14 @@ public class ReviewOrder {
 "           removedCount++;\r\n" + 
 "        }\r\n" + 
 "    }\r\n" + 
-"    console.log('removed: '+removedCount);\r\n" + 
+"    console.log('Items removed from ActiveQueue: '+removedCount);\r\n" + 
 "    \r\n" + 
 "    for(var i=revList.length-1;i>=0;i--){\r\n" + 
 "        var it=revList[i];\r\n" + 
 "        if(it.kan){\r\n" + 
 "           revList.splice(i,1);\r\n" + 
 "           revList.push(it);\r\n" + 
-"           console.log(it.kan);\r\n" + 
+"           //console.log('kan '+it.kan);\r\n" + 
 "        }\r\n" + 
 "    }\r\n" + 
 "    for(var i=revList.length-1;i>=0;i--){\r\n" + 
@@ -39,35 +68,36 @@ public class ReviewOrder {
 "        if(it.rad){\r\n" + 
 "           revList.splice(i,1);\r\n" + 
 "           revList.push(it);\r\n" + 
-"           console.log(it.rad);\r\n" + 
+"           //console.log('rad '+it.rad);\r\n" + 
 "        }\r\n" + 
 "    }\r\n" + 
 "    \r\n" + 
 "    for(var i=0;i<removedCount;i++){\r\n" + 
 "        actList.push(revList.pop());\r\n" + 
 "    }\r\n" + 
+"    \r\n" + 
+"    console.log('Ordered ReviewQueue:');\r\n" + 
+"    for(var i=0;i<revList.length;i++){\r\n" + 
+"        var it=revList[i];\r\n" + 
+"        if(it.rad)\r\n" + 
+"        	console.log('rad '+it.rad);\r\n" + 
+"        else if(it.kan)\r\n" + 
+"           	console.log('kan '+it.kan);\r\n" + 
+"        else if(it.voc)\r\n" + 
+"            console.log('voc '+it.voc);\r\n" + 
+"    }\r\n" + 
+"    \r\n" + 
 "    $.jStorage.set(\"reviewQueue\",revList);\r\n" + 
 "    $.jStorage.set(\"activeQueue\",actList);\r\n" + 
 "    \r\n" + 
-"    var stats = $(\"#stats\")[0];\r\n" + 
-"    var t = document.createElement('div');\r\n" + 
-"    stats.appendChild(t);\r\n" + 
-"    \r\n" + 
-"    t.innerHTML = '<div id=\"wkroStatus\"><table align=\"right\"><tbody>'+\r\n" + 
-"        '<tr><td>Rad</td><td align=\"right\"><span id=\"wkroRadCount\"></span></td></tr>'+\r\n" + 
-"        '<tr><td>Kan</td><td align=\"right\"><span id=\"wkroKanCount\"></span></td></tr>'+\r\n" + 
-"        '<tr><td>Voc</td><td align=\"right\"><span id=\"wkroVocCount\"></span></td></tr>'+\r\n" + 
-"        '</tbody></table></div>';\r\n" + 
-"    \r\n" + 
-"    $.jStorage.listenKeyChange(\"activeQueue\",displayUpdate);\r\n" + 
-"    displayUpdate();\r\n" + 
-"	console.log('end');\r\n" + 
+"    divSt.innerHTML = 'Done!';\r\n" + 
+"    console.log('reorder() end');\r\n" + 
 "}\r\n" + 
 "\r\n" + 
 "function displayUpdate(){\r\n" + 
 "    var radC = 0, kanC = 0, vocC = 0;\r\n" + 
 "    var list = $.jStorage.get(\"reviewQueue\").concat($.jStorage.get(\"activeQueue\"));\r\n" + 
-"    console.log(list.length);\r\n" + 
+"    console.log('ReviewQueue ('+$.jStorage.get(\"reviewQueue\").length+') ActiveQueue ('+$.jStorage.get(\"activeQueue\").length+')');\r\n" + 
 "    for(var i=0;i<list.length;i++){\r\n" + 
 "        var it=list[i];\r\n" + 
 "        if(it.rad)\r\n" + 
@@ -77,7 +107,7 @@ public class ReviewOrder {
 "        else if(it.voc)\r\n" + 
 "           	vocC++;\r\n" + 
 "    }\r\n" + 
-"    console.log(radC+\" \"+kanC+\" \"+vocC);\r\n" + 
+"    console.log('Rad '+radC+' Kan '+kanC+' Voc '+vocC);\r\n" + 
 "    var radSpan = $(\"#wkroRadCount\")[0];\r\n" + 
 "    var kanSpan = $(\"#wkroKanCount\")[0];\r\n" + 
 "    var vocSpan = $(\"#wkroVocCount\")[0];\r\n" + 
@@ -89,11 +119,13 @@ public class ReviewOrder {
 // Glue code //
 "if ($(\"#wkroStatus\").length > 0) {" +
 "    $(\"#wkroStatus\").show (); " +
+"    $(\"#divSt\").show (); " +
 "} else {" +
-"    setTimeout(init,8000);\r\n" + 
-"    console.log('load');" +
+"    init();\r\n" + 
+"    console.log('script load end');" +
 "}";			
 
 	public static final String JS_UNINIT_CODE =
-"$(\"#wkroStatus\").hide ();";
+"$(\"#wkroStatus\").hide ();" +
+"$(\"#divSt\").hide (); ";
 }
