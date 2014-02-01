7b68a801cdf32126044a113a0edc78f6e1d68593
Alberto Cuda <alberto.cuda@gmail.com>
2014-02-01 18:27:26 +0100

    Added pie plot percentages

--- src/com/wanikani/androidnotifier/graph/PieChart.java
+++ src/com/wanikani/androidnotifier/graph/PieChart.java
@@ -1,10 +1,12 @@
 package com.wanikani.androidnotifier.graph;
 
 import java.util.List;
+import java.util.Vector;
 
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.text.Html;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -52,6 +54,16 @@ public class PieChart extends IconizableChart {
 		}
 		
 	}
+	
+	private class Switcher implements View.OnClickListener {
+		
+		@Override
+		public void onClick (View view)
+		{
+			switchToValues (!valdisp);
+		}
+		
+	}
 
 	/// The real pie plot image
 	PiePlot plot;
@@ -68,6 +80,18 @@ public class PieChart extends IconizableChart {
 	/// Is data available
 	boolean available;
 	
+	/// The list of percentage views
+	List<View> percViews;
+	
+	/// The list of value views
+	List<View> valViews;
+	
+	/// The switcher
+	TextView swv;
+	
+	/// Set if we are displaying values
+	boolean valdisp;
+	
 	/**
 	 * Constructor. 
 	 * @param ctxt context
@@ -81,6 +105,32 @@ public class PieChart extends IconizableChart {
 		legend = (LinearLayout) findViewById (R.id.pc_legend);
 		alertPanel = findViewById (R.id.pc_lay_alert);
 		alertMessage = (TextView) findViewById (R.id.pc_alert);
+		
+		swv = (TextView) findViewById (R.id.pc_switcher);
+		swv.setClickable (true);
+		swv.setOnClickListener (new Switcher ());		
+		switchToValues (true);
+	}
+	
+	private void switchToValues (boolean enable)
+	{
+		List<View> v;
+		
+		valdisp = enable;
+		
+		swv.setText (Html.fromHtml ("<font color=\"blue\"><u>" + (valdisp ? "%" : "#") + "</u></font>"));
+		
+		v = !valdisp ? valViews : percViews;
+		if (v != null) {
+			for (View view : v)
+				view.setVisibility (View.GONE);
+		}
+
+		v = valdisp ? valViews : percViews;
+		if (v != null) {
+			for (View view : v)
+				view.setVisibility (View.VISIBLE);
+		}
 	}
 	
 	/**
@@ -92,31 +142,39 @@ public class PieChart extends IconizableChart {
 	public void setData (List<DataSet> dsets, List<InfoSet> infosets, int anchor)
 	{
 		LinearLayout item;		
+		float total;
 		int i;
 		
 		plot.setData (dsets);
 		
+		total = 0;
+		for (DataSet ds : dsets)
+			total += ds.value;
+					
 		legend.removeAllViews ();
+		percViews = new Vector<View> ();
+		valViews = new Vector<View> ();
+
 		i = 0;
 		for (DataSet ds : dsets) {
 			if (i++ == anchor)
-				addInfosets (infosets);
+				addInfosets (infosets, total);
 			
 			if (ds.value > 0) {
 				item = (LinearLayout) inflater.inflate (R.layout.legend, null); 
-				customizeItem (item, ds, true);
+				customizeItem (item, ds, total, true);
 				legend.addView (item);
 			}
 		}
 		
 		if (i <= anchor)
-			addInfosets (infosets);
+			addInfosets (infosets, total);
 		
 		available = true;
 		dataAvailable ();		
 	}
 	
-	private void addInfosets (List<InfoSet> infosets)
+	private void addInfosets (List<InfoSet> infosets, float total)
 	{
 		LinearLayout item;		
 
@@ -124,7 +182,7 @@ public class PieChart extends IconizableChart {
 			for (DataSet ds : infosets) {
 				if (ds.value > 0) {
 					item = (LinearLayout) inflater.inflate (R.layout.legend, null); 
-					customizeItem (item, ds, false);
+					customizeItem (item, ds, total, false);
 					legend.addView (item);
 				}
 			}			
@@ -135,12 +193,21 @@ public class PieChart extends IconizableChart {
 	 * Fills a new legend item.
 	 * @param item the item to fill
 	 * @param ds the dataset the item shall describe
+	 * @param total the sum of all the datasets
 	 * @param color if set, add a palette
 	 */
 	@SuppressWarnings ("deprecation")
-	protected void customizeItem (LinearLayout item, DataSet ds, boolean color)
+	protected void customizeItem (LinearLayout item, DataSet ds, float total, boolean color)
 	{
 		Drawable tag;
+		TextView tv;
+		
+		if (total > 0) {
+			valViews.add (item.findViewById (R.id.leg_value));
+			tv = (TextView) item.findViewById (R.id.leg_percentage);
+			percViews.add (tv);
+			tv.setText (Math.round (100 * ds.value / total) + "%");
+		}
 		
 		if (color) {
 			tag = new ColorDrawable (ds.color);
